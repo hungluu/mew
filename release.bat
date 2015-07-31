67552de18deb7886cca95f09a6a432b771f54a3c
madle <luuhug@gmail.com>
2015-08-01 00:57:17 +0700

    First commit

---
+++ release.bat
@@ -0,0 +1,26 @@
+@echo off
+SET modulelist=functions
+SET release=release\mew
+if not exist %modulelist% goto nodir
+type nul > %release%
+for /f %%a in (tib-list) do (
+	if not exist %modulelist%\%%a (
+		echo MODULE %%a NOT FOUND
+		goto fail
+	)
+
+	echo COPYING MODULE %%a TO RELEASE FILE
+	type %modulelist%\%%a >> %release%
+)
+goto success
+:success
+echo SUCCESS
+goto end
+:fail
+goto end
+:nodir
+echo NO MODULE DIRECTORY FOUND.
+goto end
+:end
+pause
+exit
